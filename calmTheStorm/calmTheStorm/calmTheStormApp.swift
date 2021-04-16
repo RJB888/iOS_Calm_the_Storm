0d402e71079ffc91f859c680bbe575dc69d10707
rjb888 <robert.j.bronson@gmail.com>
2021-04-15 18:16:38 -0700

    Initialized swift project and first commit

---
+++ calmTheStorm/calmTheStorm/calmTheStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  calmTheStormApp.swift
+//  calmTheStorm
+//
+//  Created by ROBERT BRONSON on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct calmTheStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
