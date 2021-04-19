cf7d7a4aed8b271cd26f6a7c1e03677f47b424e0
Matin Mirzaei <mirzaei.matin@yahoo.com>
2021-04-18 21:43:08 -0700

    update and add icons to the main disasters page

--- calmTheStorm/calmTheStorm/ContentView.swift
+++ calmTheStorm/calmTheStorm/ContentView.swift
@@ -6,18 +6,54 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct ContentView: View {
     var body: some View {
         
         ZStack{
             Color.Ivory.ignoresSafeArea(.all)
-            Text("Hello, Calm the Storm Team!")
+        VStack{
+        HStack{
+            Text("Calm The Storm")
                 .font(
                     //Is This Font Okay?
-                    .custom("Avenir", size: 20))
-
-                .padding()
+                    .custom("Avenir", size: 25))
+                .bold()
+                .padding(.top)
+                
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 30))
+           
+                }
+            VStack{
+                Image(systemName: "tornado")
+                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 40))
+                    .padding(.top,80)
+                
+                Image(systemName: "flame.fill")
+                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 40))
+                    .padding()
+                
+                Image(systemName: "waveform.path.ecg")
+                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 40))
+                    .padding(.bottom, 30)
+                
+                Image(systemName: "cloud.fill")
+                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 32))
+                    .padding(-20)
+                
+                Image(systemName: "triangle")
+                    .font(.system(size: 40))
+                    .padding(-12)
+            }
+            Spacer()
+            }
         }
     }
 }
