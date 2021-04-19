28f6b3b38f94e46015320dff8256fe89a79406a3
https://github.com/JustinW1395/iOS-Chapter-01/tree/quiz-swift/Quiz <just>
2021-04-18 19:43:18 -0700

    Added two new custom colors, set the background color, and changed the font

---
+++ calmTheStorm/calmTheStorm/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  calmTheStorm
+//
+//  Created by Student Account on 4/18/21.
+//
+
+import SwiftUI
+
+extension Color{
+    public static let Ivory: Color = Color(UIColor(red: 255/255, green: 255/255, blue: 230/255, alpha: 1.0))
+}
+
+extension Color{
+    public static let Salmon: Color = Color(UIColor(red: 250/255, green: 128/255, blue: 114/255, alpha: 1.0))
+}
