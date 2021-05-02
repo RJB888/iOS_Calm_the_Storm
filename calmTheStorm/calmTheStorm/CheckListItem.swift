a8897a5f1ac8da356e8c9bb41af0127825daf0e7
https://github.com/JustinW1395/iOS-Chapter-01/tree/quiz-swift/Quiz <just>
2021-05-01 19:47:20 -0700

    Added a checkboxes to the app, and started the survival kit list

---
+++ calmTheStorm/calmTheStorm/CheckListItem.swift
@@ -0,0 +1,14 @@
+//
+//  CheckListItem.swift
+//  calmTheStorm
+//
+//  Created by Student Account on 5/1/21.
+//
+
+import SwiftUI
+
+struct CheckListItem: Identifiable{
+    var id: Int
+    var isChecked: Bool = false
+    var title: String
+}
---
+++ calmTheStorm/calmTheStorm/CheckListView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckListView.swift
+//  calmTheStorm
+//
+//  Created by Student Account on 5/1/21.
+//
+
+import SwiftUI
+
+struct CheckListView: View{
+    var body: some View{
+        List(checkListData){item in
+            CheckView(isChecked: item.isChecked, title: item.title)
+        }
+        .font(.title)
+    }
+}
+
+struct CheckList_Previews: PreviewProvider{
+    static var previews: some View{
+        CheckListView()
+    }
+}
