f4e70a1bfd8afd5205fcf639276e90aa5af84109
https://github.com/JustinW1395/iOS-Chapter-01/tree/quiz-swift/Quiz <just>
2021-05-19 14:09:22 -0700

    Added disaster info items, and the disaster info page

---
+++ calmTheStorm/calmTheStorm/DisasterType.swift
@@ -0,0 +1,15 @@
+//
+//  DisasterType.swift
+//  calmTheStorm
+//
+//  Created by Student Account on 5/17/21.
+//
+
+import Foundation
+
+enum DisasterTypeMode{
+    case Tornado
+    case Wildfire
+    case Earthquake
+    case Volcano
+}
---
+++ calmTheStorm/calmTheStorm/MainDisasterInfo.swift
@@ -0,0 +1,49 @@
+//
+//  MainDisasterInfo.swift
+//  calmTheStorm
+//
+//  Created by Student Account on 5/19/21.
+//
+
+import SwiftUI
+
+
+struct MainDisasterInfo: View {
+    
+    var DisasterInfo: String
+    var body: some View {
+        VStack{
+            Text("\(DisasterInfo) Information")
+            
+            if(DisasterInfo == "Tornado"){
+                List(TornadoData){ Info in
+                    infoRow(whichInfo: Info)
+                }
+            }
+            
+            if(DisasterInfo == "Wildfire"){
+                List(WildfireData){ Info in
+                    infoRow(whichInfo: Info)
+                }
+            }
+            
+            if(DisasterInfo == "Volcano"){
+                List(EarthquakeData){ Info in
+                    infoRow(whichInfo: Info)
+                }
+            }
+            
+            if(DisasterInfo == "Earthquake"){
+                List(VolcanoData){ Info in
+                    infoRow(whichInfo: Info)
+                }
+            }
+        }
+    }
+}
+
+struct MainDisasterInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        MainDisasterInfo(DisasterInfo: "test")
+    }
+}
